2e7f06e6fc09b47b9895bd28e90e6d7639fb4059
minealicer <[milisil@uoc.edu]>
2021-11-30 11:22:37 +0100

    enter

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,13 +6,3 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 import * as bootstrap from 'bootstrap';
-
-/**
- * Write any other JavaScript below
- */
-
-+
-(function() {
-    const university = "UOC";
-    console.log(`Hello, ${university}!`);
-})();
